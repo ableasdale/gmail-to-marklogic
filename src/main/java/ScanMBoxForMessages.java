594596bbd8760b387733379802dee9aa9b2e192d
Alex Bleasdale <ableasdale@marklogic.com>
2018-03-07 16:55:49 +0000

    Initial WIP

---
+++ src/main/java/ScanMBoxForMessages.java
@@ -0,0 +1,46 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class ScanMBoxForMessages {
+
+    private static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    static int count = 0;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        String fileName = "C:\\Users\\alexb\\Downloads\\All mail Including Spam and Trash-021.mbox";
+
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+
+            //stream.forEach(doSomething());
+            stream.forEach(s -> doSomething(s));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        LOG.info("Total"+count);
+    }
+
+    private static void doSomething(String s) {
+        if(s.startsWith("From") ) {
+            if(sb.length() > 0){
+                // At this point we have an email to parse
+                LOG.info(sb.toString());
+
+                sb.setLength(0);
+            }
+            count++;
+        }
+        sb.append(s);
+    }
+}
---
+++ src/main/java/TikaMboxReader.java
@@ -0,0 +1,63 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.tika.detect.TypeDetector;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.RecursiveParserWrapper;
+import org.apache.tika.parser.mbox.MboxParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+
+public class TikaMboxReader {
+
+    private static Logger LOG = LoggerFactory.getLogger("x");
+
+    private static InputStream getStream(String name) {
+        return TikaMboxReader.class.getClass().getResourceAsStream(name);
+    }
+
+    public static void main(String[] args) {
+
+        ParseContext recursingContext;
+        Parser autoDetectParser;
+        TypeDetector typeDetector;
+        MboxParser mboxParser;
+
+
+            typeDetector = new TypeDetector();
+            autoDetectParser = new AutoDetectParser(typeDetector);
+            recursingContext = new ParseContext();
+            recursingContext.set(Parser.class, autoDetectParser);
+
+            mboxParser = new MboxParser();
+            mboxParser.setTracking(true);
+
+            ContentHandler handler = new BodyContentHandler(-1);
+            Metadata metadata = new Metadata();
+
+            try (InputStream stream = new FileInputStream("C:\\Users\\alexb\\Downloads\\All mail Including Spam and Trash-021.mbox")) {
+                mboxParser.parse(stream, handler, metadata, recursingContext);
+                Map<Integer, Metadata> mailsMetadata = mboxParser.getTrackingMetadata();
+                LOG.info("Map size"+mailsMetadata.size());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TikaException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
+            }
+    }
+}
